5a0c581d711e135865252275b785aa09194fefb1
Lê Thanh Nin <88334367+LETHANHNIN@users.noreply.github.com>
2026-07-31 09:25:33 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("time").textContent =
-  "Trang được tải lúc: " + new Date().toLocaleString("vi-VN");
-
-let count = 0;
-const countEl = document.getElementById("count");
-document.getElementById("btn").addEventListener("click", () => {
-  count++;
-  countEl.textContent = "Đã bấm: " + count + " lần";
-});
